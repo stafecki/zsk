880e42a0ce4af8fccadbc7be98496cdb58c7da89
stafecki <90975231+stafecki@users.noreply.github.com>
2023-09-28 07:50:19 +0200

    Program.cs

--- project_2_1/project_2_3/Program.cs
+++ project_2_1/project_2_3/Program.cs
@@ -29,6 +29,18 @@ namespace project_2_3
              * uint 0 do 4 294 967 295 (32 bity = 4 bajty, Int32)
              * 
              * Int64 = System.Int64 = long
+             *
+             * long -9 223 372 036 854 775 808 do 9 223 372 036 854 775 807 (64 bity = 8 bajtów)
+             * ulong 0 do 18 446 744 073 709 551 615 (64 bity = 8 bajtów)
+             * 
+             * float -3,4 * 10^38 do 3,4 * 10^38 (32 bity = 4 bajty)
+             * double (64 bity = 8 bajtów)
+             * decimal (128 bitów = 32 bajty)
+             * 
+             * char U+0000 do U+FFFF (16 bitowy znak z tablicy Unicode)
+             * string
+             *
+             * bool (1 bajt)
              */
             byte b = 0;
             Console.WriteLine("Zmienna b wynosi: " + b);
@@ -44,6 +56,15 @@ namespace project_2_3
             Int16 i1 = -32767; //to samo so short
 
             System.Int64 i2 = 22222222222222;
+
+            int i2 = 10;
+            Int32 i3 = 10;
+            System.Int32 i4 = 10;
+
+            Console.WriteLine("Rozmiar typu bool" +sizeof(bool));
+
+            float f = 10.5F;
+            Console.WriteLine(f);
             
             Console.ReadKey();
         }
