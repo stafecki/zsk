b89d2f29b551353e7fc11dbcb0b9bf680c875b0c
stafecki <90975231+stafecki@users.noreply.github.com>
2024-03-07 08:41:53 +0100

    Add files via upload

---
+++ konstruktory2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using konstruktory2.classess;
+namespace konstruktory2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p0 = new Person();
+            Person p1 = new Person("Jan");
+            Person p2 = new Person("Jan", "Nowak");
+            Person p3 = new Person("Jan", "Nowak", 20);
+            Person p4 = new Person("Jan", "Nowak", 20, 175.5f);
+            /*Console.WriteLine($"Ilość obiektów klasy Person: {Person.Counter}");*/
+            List<Person> people = new List<Person>();
+            /*people.Add(p0);
+            people.Add(p1);
+            people.Add(p2);
+            people.Add(p3);
+            people.Add(p4);*/
+            people.AddRange(new[] { p0, p1, p2, p3, p4 });
+            Console.WriteLine($"Ilość obiektów klasy Person: {people.Count}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ konstruktory2/classess/Person.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace konstruktory2.classess
+{
+    internal class Person
+    {
+        //statyczne pola które przechowuje liczbę obieków klasy person
+        public static int Counter = 0;
+
+        //właściwości
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public float Height { get; set; }
+
+        /*konstruktor statyczne - jest on wywoływany automatycznie aby zainicjowac klasę przed utworzeniem pierszej instancji.
+        Konstruktor statyczny jest wywoływany tylko raz przed pierwszym użyciem.
+        Nie może mieć parametrów ani modyfikatorów dostępu
+        Służy do inicjowania pól statycznych lub wykonywania innych operacji statycznych(wykonywanie kodu który jest związany z klasą a nie z jej obiektami np. ustawianie wartości domyślnych dla pól statycznych)*/
+        static Person()
+        {
+            Console.WriteLine("Statyczny konstruktor klasy Person\n");
+            //Counter++;
+        }
+
+        //konstruktor domyślny jest bezparametrowy. Jeśłi klasa nie ma żadnego konstruktora parametrycznego, to konstruktor domyślny jest wywoływany przy tworzeniu obiektu. Inicjuje on wszystkie pola do ich wartości domyślnych. Jeśli zdefiniujemy jakiś konstruktor parametryczny, to nie otrzymamy automatycznie konstruktora domyślnego i możemy/musimy go samodzielnie zadeklarować
+        public Person()
+        {
+            Console.WriteLine("Konstruktor domyślny klasy Person");
+            Counter++;
+        }
+        //konstruktor parametryczny ma co najmniej jeden parametr. Służy do inicjowania pól obiektu zgodnie z wartościami podanymi przy tworzeniu obiektu. Możemy mieć wiele konstruktorów parametrycznych, o ile różnią się liczbą lub/i typem parametrów 
+
+        //konstruktor parametryczny klasy person z jednym parametrem
+        public Person(string name)
+        {
+            Console.WriteLine("Konstruktor parametryczny z jednym parametrem");
+            Name = name;
+            Counter++;
+        }
+
+        //konstruktor klasy Person z dwoma parametrami
+        public Person(string name, string surname)
+        {
+            Console.WriteLine("Konstruktor parametryczny z dwoma parametrami");
+            Name = name;
+            Surname = surname;
+            Counter++;
+        }
+
+        //konstruktor klasy Person z trzema parametrami
+        //this służy do wywołania innego konstruktora tej samej klasy, cyzli konstruktora z dwoma parametrami. Dzięki temu konstruktor z trzema parametrami nie musi inicjować pól Name, Surname a może skupić się na dodaniu pola Age. Jest to sposób na uniknięcie powtarzania kodu i zapewnienie spójności danych
+        public Person(string name, string surname, int age) : this(name, surname) 
+        {
+            Console.WriteLine("Konstruktor parametryczny z trzema parametrami");
+            Age = age;
+            Counter++;
+        }
+
+        //konstruktor klasy Person z czterema parametrami
+        public Person(string name, string surname, int age, float height) : this(name, surname, age)
+        {
+            Console.WriteLine("Konstruktor parametryczny z czterema parametrami");
+            Height = height;
+            Counter++;
+        }
+    }
+}
