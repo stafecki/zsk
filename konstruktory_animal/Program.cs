b1c61dc3f8141392a6c90aed78f784c3e862792f
stafecki <90975231+stafecki@users.noreply.github.com>
2024-03-14 08:42:06 +0100

    Add files via upload

---
+++ konstruktory_animal/Program.cs
@@ -0,0 +1,96 @@
+﻿using System.Formats.Asn1;
+using konstruktory_animal.classess;
+namespace konstruktory_animal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region animalObjects
+            /*Animal a1 = new Animal("Burek");
+            Console.WriteLine(a1.Describe());
+            a1.BirthDate = new DateTime(2000, 2, 3);
+            a1.ShowAge();*/
+
+            /*Animal a2= new Animal("Mruczek", new DateTime(2020,1,10));
+            Console.WriteLine(a2.Describe());
+            a2.BirthDate = new DateTime(2020, 2, 3);
+            a2.ShowAge();*/
+
+            /*Animal a3 = new Animal("Kotomiler", new DateTime(2020,1,10), true);
+            Console.WriteLine(a3.Describe());
+            a3.BirthDate = new DateTime(2020, 2, 3);
+            a3.ShowAge();*/
+
+            /*Animal a4 = new Animal("Bobek", new DateTime(2020, 1, 10), true, Kind.Ssak);
+            Console.WriteLine(a4.Describe());
+            a4.BirthDate = new DateTime(2020, 2, 3);
+            a4.ShowAge();*/
+            #endregion
+
+            List<Animal> animals = new List<Animal>();
+
+            ShowMainMenu(animals);
+
+            Console.ReadKey();
+        }
+
+        private static void ShowMainMenu(List<Animal> animals)
+        {
+            Console.Clear();
+            Console.WriteLine("Witaj w programie do zarządzania zwierzętami");
+            Console.WriteLine("1. Dodaj zwierze");
+            Console.WriteLine("2. Pokaż listę zwierząt");
+            Console.WriteLine("3. Pokaz szczegóły zwierzęcia");
+            Console.WriteLine("4. Usuń zwierze");
+            Console.WriteLine("5. Zakończ program\n");
+            Console.Write("Wybierz jedną z opcji: ");
+
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    AddNewAnimal(animals);
+                    break;
+                case "2":
+                    ShowAnimalList(animals); 
+                    break;
+                case "3":
+                    ShowAnimalDetails(animals);
+                    break;
+                case "4":
+                    RemoweAnimal(animals);
+                    break;
+                case "5":
+                    Console.WriteLine("Dziękujemy za skorzystanie z programu");
+                    return;
+                default:
+                    Console.WriteLine("Niepoprawna opcja, naciśnij dowolny klawisz aby spróbowac ponownie");
+                    Console.ReadKey();
+                    ShowMainMenu(animals);
+                    break;
+            }
+        }
+
+        private static void RemoweAnimal(List<Animal> animals)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void ShowAnimalDetails(List<Animal> animals)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void ShowAnimalList(List<Animal> animals)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void AddNewAnimal(List<Animal> animals)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ konstruktory_animal/classess/Animal.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace konstruktory_animal.classess
+{
+    enum Kind
+    {
+        Ptak,
+        Ryba,
+        Gad,
+        Płaz,
+        Ssak
+    }
+    internal class Animal
+    {
+        //WŁAŚCIWOŚCI
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public bool IsMammal { get; set; }
+        public Kind Kind { get; set; }
+        //KONSTRUKTORY--------------------------------------------------------------------------
+        public Animal(string name)
+        {
+            Name = name;
+        }
+        public Animal(string name, DateTime birthDate) : this(name)
+        {
+            BirthDate = birthDate;
+        }
+        public Animal(string name, DateTime birthDate, bool isMammal) : this(name, birthDate)
+        {
+            IsMammal = isMammal;
+        }
+        public Animal(string name, DateTime birthDate, bool isMammal, Kind kind) : this(name, birthDate, isMammal)
+        {
+            Kind = kind;
+        }
+        //METODY
+        public string Describe()
+        {
+            string description = "Nazwa zwierzęcia: " + Name + ",\nData urodzenia: " + BirthDate.ToShortDateString() + "r., ";
+            if (IsMammal)
+            {
+                description += "\nZwierze jest ssakiem,";
+            }
+            else
+            {
+                description += "\nZwierze nie jest ssakiem,";
+            }
+            description += "\nRodzaj zwierzęcia: " + Kind;
+
+            return description;
+        }
+        public void ShowAge()
+        {
+            int age = DateTime.Now.Year - BirthDate.Year;
+            Console.WriteLine($"Wiek {Name} wynosi {age}");
+        }
+        
+    }
+}
